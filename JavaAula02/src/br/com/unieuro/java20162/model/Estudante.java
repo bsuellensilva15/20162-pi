0f7a8a3abdb2aa56d853a7ea2ccedd59cc38cf6b
aluno <aluno@LAB4C2-28.laboratorio.int>
2016-08-29 20:43:36 -0300

    Commit Inicial do projeto 20162-pi.
    Conversão do projeto Java para projeto Maven.
    Configuração do maven para importar o JDBC do MySQL.
    Criação da classe que representa Disciplina.
    Criação da classe que representa Estudante.
    Criação da classe que representa Grade.
    Criação da classe que representa Nota.
    Criação da classe que representa PreRequisito.

---
+++ JavaAula02/src/br/com/unieuro/java20162/model/Estudante.java
@@ -0,0 +1,11 @@
+package br.com.unieuro.java20162.model;
+
+public class Estudante {
+	private String nome_do_estudante;
+	private Integer id_estudante;
+	private String senioridade;
+	private String graduacao;
+	
+	public Estudante(String nome_do_estudante, )
+
+}
